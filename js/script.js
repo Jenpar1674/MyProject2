42b082710856927a66fdd409974c8f52d6e9e530
Jenpar1674 <51331242+Jenpar1674@users.noreply.github.com>
2019-06-21 01:00:43 -0500

    final commit rfor evening

--- js/script.js
+++ js/script.js
@@ -91,8 +91,9 @@ const pageAppend =(list)=>{
       
    
    
-      document.addEventListener('click',()=>{
+      a.addEventListener('click',()=>{
       
+         
          console.log(EventTarget);
 
          
